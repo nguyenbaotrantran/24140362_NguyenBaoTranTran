d0c79eb808b744380f32dca9d672f1bc90949e90
nguyenbaotrantran <nguyenbaotrantran56@gmail.com>
2026-07-04 10:43:08 +0700

    Add files via upload

---
+++ lap1chuong1/src/main/java/vn/edu/student/Main.java
@@ -0,0 +1,9 @@
+package vn.edu.student;
+
+public class Main {public static void main(String[] args) {
+
+    System.out.println("Hello Maven!");
+    System.out.println("Xin chào Java hiện đại!");
+
+ }
+}
