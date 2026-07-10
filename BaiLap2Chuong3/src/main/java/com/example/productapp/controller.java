41eb34989951a8ed84b57122315e7384d826bac7
nguyenbaotrantran <nguyenbaotrantran56@gmail.com>
2026-07-10 10:15:33 +0700

    Add files via upload

---
+++ BaiLap2Chuong3/src/main/java/com/example/productapp/controller.java
@@ -0,0 +1,6 @@
+package com.example.productapp;
+
+public interface controller {
+    class ProductController {
+    }
+}
---
+++ BaiLap2Chuong3/src/main/java/com/example/productapp/model.java
@@ -0,0 +1,4 @@
+package com.example.productapp;
+
+public interface model {
+}
